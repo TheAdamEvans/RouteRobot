41df7c1c9a0ead347db973db26263b7d48eec9e1
TheAdamEvans <theadamevans@gmail.com>
2016-03-11 19:59:53 -0700

    Working scraper object

--- obj_scrape.py
+++ obj_scrape.py
@@ -1,16 +1,31 @@
 from Scraper import Scraper
 from Destination import Destination
+import json
 
 def traverse(node):
     children = []
     for href in node.children_href:
+
+        # call for this page and store the soup
         scrap = Scraper(href)
+
+        # parse relevant infomation from the page
         dest = scrap.create_destination()
+
+        # look for children of this route
         dest.children_href = scrap.get_children()
-        if dest.children_href != None:
-            traverse_routes(dest)
-        children.append(dest)
+
+        # recursively deeper down the tree
+        if dest.children_href is not None:
+            traverse(dest)
+        else:
+            # at the route level (leafs)
+            print dest.href
+            children.append(dest)
+
+    # set the destination objects as children
     node.children = children
+
     return node
 
 # initialize root destination with children
@@ -23,9 +38,10 @@ dest.children_href = scrap.get_children()
 all_dest = traverse(dest)
 # returns Destination object
 
-
 # write out to file
 BIG_JSON = './mp_tree.json'
 with open(BIG_JSON, 'a') as dump:
     flat = json.dumps(all_dest, default=lambda o: o.__dict__)
     dump.write(flat)
+
+print "Printed " + BIG_JSON
