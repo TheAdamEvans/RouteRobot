a3f8faf2e5bb5e66bbe4896dbb38af8b2af291f3
dean.wyatte <dean.wyatte@logrhythm.com>
2016-04-04 08:50:02 -0600

    actually learns in finite time on CPU

--- NameGenerator/lstm.py
+++ NameGenerator/lstm.py
@@ -1,45 +1,40 @@
+import re
 import sys
+import random
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Activation
-from keras.optimizers import Adagrad
+from keras.optimizers import RMSprop
 
 # starting params
 hidden1_size = 128
-seq_len = 25
+seq_len = 5
 nb_epoch = 25
 
-# get just the route names -- will need to clean this data further
-# to remove space characters and information after route name
-# preprocessed with strings, find and delete " ,
-df = pd.read_csv('route_name.txt', header=None)
-df = df.dropna()
-print "read %d route names" % df.shape[0]
+# get just the route names
+df = pd.read_csv('summary_data.csv')
+
+# basic cleaning to validate this works. english + select chars only, lowercase, single space
+names = df['name'][df['is_route']].dropna().values
+names = [name for name in names if re.search('^[A-Za-z0-9 !@#$%^&*_-]*$', name)]
+names = [name.lower() for name in names]
+names = [' '.join(name.split()) for name in names]
+text = '\n'.join(names)
 
 # vocabulary
-text = ''.join(df[0].tolist())
 chars = set(text)
 print 'vocab:', chars
 char_to_idx = dict((c, i) for i, c in enumerate(chars))
 idx_to_char = dict((i, c) for i, c in enumerate(chars))
 
-def print_prediction(prediction):
-    for p in prediction:
-        m = max(p)
-        index = [i for i, j in enumerate(p) if j == m]
-        max_likely_letter = idx_to_char[index[0]]
-        sys.stdout.write(max_likely_letter)
-
-# vectorize data by chunking into overlapping sequences of equal length
-# there is a way to use variable-length seqs with rnns, so we could use the individual names
-maxlen = 20
+# vectorize data
 step = 3
 seqs = []
 next_chars = []
-for i in range(0, len(text) - maxlen, step):
-    seqs.append(text[i: i + maxlen])
-    next_chars.append(text[i + maxlen])
+for i in range(0, len(text) - seq_len, step):
+    seqs.append(text[i: i + seq_len])
+    next_chars.append(text[i + seq_len])
 X = np.zeros((len(seqs), seq_len, len(chars)), dtype=np.bool)
 y = np.zeros((len(seqs), len(chars)), dtype=np.bool)
 for i, seq in enumerate(seqs):
@@ -47,25 +42,55 @@ for i, seq in enumerate(seqs):
         X[i, t, char_to_idx[char]] = 1
     y[i, char_to_idx[next_chars[i]]] = 1
 
+
 # model def -- single hidden layer for now
 model = Sequential()
 model.add(LSTM(hidden1_size, return_sequences=False,  input_shape=(seq_len, len(chars))))
 model.add(Dense(len(chars)))
 model.add(Activation('softmax'))
-opt = Adagrad()
+opt = RMSprop()
 model.compile(loss='categorical_crossentropy', optimizer=opt)
 
+
+def sample(a, temperature=1.0):
+    # helper function to sample an index from a probability array
+    a = np.log(a) / temperature
+    a = np.exp(a) / np.sum(np.exp(a))
+    return np.argmax(np.random.multinomial(1, a, 1))
+
+
 # can sample from model in this loop
-p = 0
 for epoch in range(nb_epoch):
     print
     print '-' * 50
     print 'Epoch ' + str(epoch)
-    model_hist = model.fit(X, y, nb_epoch=nb_epoch)
+    model_hist = model.fit(X, y, nb_epoch=1)
 
-    # attempt to see output
-    # needs to loop on itself???
-    seed = X[:1000]
-    prediction = model.predict(seed)
-    print_prediction(prediction)
+    start_index = random.randint(0, len(text) - seq_len - 1)
+
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print()
+        print('----- diversity:', diversity)
+
+        generated = ''
+        sentence = text[start_index: start_index + seq_len]
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+
+        for i in range(400):
+            x = np.zeros((1, seq_len, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_to_idx[char]] = 1.
+
+            preds = model.predict(x, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = idx_to_char[next_index]
+
+            generated += next_char
+            sentence = sentence[1:] + next_char
+
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
 
