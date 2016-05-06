d71fa5e464995a49290b432256a8d3695a70bc5e
TheAdamEvans <theadamevans@gmail.com>
2016-05-05 21:29:44 -0600

    Copies files over to web folder

--- scraping/driver.py
+++ scraping/driver.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 import obj_scrape
-
+from grade_cleaner import create_grade_map
 from save_better import cast_all_pickles
 from text_mining import process_tokens
 from collapse import collapse_hierarchy
@@ -27,30 +27,32 @@ SCHEMA = [
 
 if __name__ == '__main__':
 
-    # print "Scraping..."
+    # print 'Scraping...'
     # obj_scrape.scrape_all(ROOT_HREF, DATA_DIR)
     
-    print "Casting..."
+    print 'Casting...'
     climb = cast_all_pickles(DATA_DIR)
 
     print 'Vectorizing...'
     vocab = pd.read_csv(VOCAB, header=None)[0].tolist()
     climb, idf_lookup = process_tokens(climb, vocab)
     # save IDF for arbitrary text queries later
-    pickle.dump(idf_lookup, open(DATA_DIR + 'idf', 'wb'))
+    pickle.dump(idf_lookup, open(DATA_DIR+'idf', 'wb'))
 
     print 'Collapsing hierarchy...'
     climb = collapse_hierarchy(climb)
 
     print 'Mapping grades...'
-    # grade_map = create_grade_map(climb, rating_system = ['rateHueco','rateYDS'])
+    grade_map = create_grade_map(climb, rating_system = ['rateHueco','rateYDS'])
+    pickle.dump(grade_map, open(DATA_DIR+'grade_map', 'wb'))
 
-    print "Saving data..."
+    print 'Saving data...'
     # save entire object
     climb.to_pickle(DATA_DIR+'_climb')
     # save database-friendly only columns
-    climb[SCHEMA].to_pickle(DATA_DIR+'climb.pickle')
+    climb[SCHEMA].to_pickle(DATA_DIR+'climb')
     climb[SCHEMA].to_csv(DATA_DIR+'climb.csv', header=True, index=None)
 
-    print 'Copying objects to route-web folder...'
-    copyfile(DATA_DIR+'climb.pickle', '../../route-web/climb.pickle')
+    for web_file in ['idf','grade_map','climb']:
+        print 'Copied to route-web/data/'+web_file
+        copyfile(DATA_DIR+web_file, '../../route-web/data/'+web_file)
